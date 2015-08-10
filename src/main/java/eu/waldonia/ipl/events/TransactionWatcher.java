39d81722ad6c6ee4e8c4d10f4dd3e40acaf55257
Sid Haniff <syed_haniff@hotmail.com>
2015-08-10 20:57:13 +0100

    Add in save on save and output on commit

--- src/main/java/eu/waldonia/ipl/events/TransactionWatcher.java
+++ src/main/java/eu/waldonia/ipl/events/TransactionWatcher.java
@@ -2,6 +2,7 @@ package eu.waldonia.ipl.events;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.session.Session;
 import org.neo4j.ogm.session.transaction.*;
 import org.slf4j.*;
@@ -10,21 +11,24 @@ import org.springframework.stereotype.Component;
 
 import eu.waldonia.ipl.populator.RosterFileProcessor;
 
+/**
+ * Not thread-safe
+ * @author sid
+ */
 @Aspect
 @Component
 public class TransactionWatcher {
 
-	Logger logger = LoggerFactory.getLogger(TransactionWatcher.class);
+	Logger logger = LoggerFactory.getLogger("TransactionWatcher");
 	
-	@Around("execution(* org.springframework.data.neo4j.transaction.Neo4jTransactionManager.commit(..))")
-	public void springCommit(ProceedingJoinPoint pjp) throws Throwable {
-		
-		Object[] args = pjp.getArgs();
-		Neo4jTransactionStatus txStatus = (Neo4jTransactionStatus)args[0];
-		Transaction tx = txStatus.getTransaction();
-		LongTransaction lt = (LongTransaction)tx;
-		System.out.println("Coooooo "+tx.getClass());
-		pjp.proceed(args);
+	Object itemToBeSaved;
+	
+	@After("execution(* org.springframework.data.neo4j.transaction.Neo4jTransactionManager.commit(..))")
+	public void springCommit() throws Throwable {
+		if (itemToBeSaved != null) {
+			logger.info("Just committed item of class "+itemToBeSaved.getClass()+" and id ");			
+		}
+		itemToBeSaved = null;
 	}
 	
 	@Around("execution(* org.neo4j.ogm.session.Session.save(..))")
@@ -33,6 +37,7 @@ public class TransactionWatcher {
 		Object[] args = pjp.getArgs();
 		Session s = (Session)pjp.getThis();
 		logger.info("Saving "+args[0].getClass()+" with attrs ");
+		itemToBeSaved = args[0];
 		pjp.proceed(args);
 	}
 	
