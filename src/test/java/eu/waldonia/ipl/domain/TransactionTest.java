e6d32ccf0a03338691d22ab101cb00f9491ad727
Sid Haniff <syed_haniff@hotmail.com>
2015-08-31 13:01:06 +0100

    Add weave time dependencies to aspectj plugin conf to pick up third party files

--- src/test/java/eu/waldonia/ipl/domain/TransactionTest.java
+++ src/test/java/eu/waldonia/ipl/domain/TransactionTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.neo4j.ogm.session.Session;
 import org.neo4j.ogm.testutil.WrappingServerIntegrationTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
